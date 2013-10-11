8da57f201bb3729113aba1a21cbbb8c400c8fa9c
sadjehwty <sadjehwty@gmail.com>
2013-10-11 18:47:35 +0200

    spostato codice nella giusta gerarchia

--- src/dev/macrobug/asmodeo/core/interfaces/Coder.java
+++ src/dev/macrobug/asmodeo/core/interfaces/Coder.java
@@ -7,16 +7,10 @@ public abstract class Coder<T> {
 	public abstract T decode(String s);
 	
 	protected Alphabet alphabet;
-	protected String charPadding;
-	protected boolean padding;
 	
 	public Coder(String s){ this(new Alphabet(s)); }
 	public Coder(Alphabet alphabet2) {this(alphabet2,alphabet2.getMax());  }
-	public Coder(Alphabet alphabet2, int max) {this(alphabet2.crop(max),true);}
-	public Coder(Alphabet alphabet,int max, boolean b){this(alphabet.crop(max),b);}
-	public Coder(Alphabet alphabet2, boolean b) {
-		alphabet=alphabet2;
-		padding=b;
-		charPadding=""+alphabet2.charAt(0);
-  }
+	public Coder(Alphabet alphabet2, int max) {
+		alphabet=alphabet2.crop(max);
+	}
 }
