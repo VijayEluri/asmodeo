021ce63e931f6e479af2828bd246b0b259bb6b86
sadjehwty <sadjehwty@gmail.com>
2013-11-14 19:41:59 +0100

    corretto codecoutputstream

--- src/dev/macrobug/asmodeo/io/CodecOutputStream.java
+++ src/dev/macrobug/asmodeo/io/CodecOutputStream.java
@@ -1,18 +1,21 @@
 package dev.macrobug.asmodeo.io;
 
-import java.io.*;
-import dev.macrobug.asmodeo.core.IntegerCoder;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import dev.macrobug.asmodeo.core.StringCoder;
 
 public class CodecOutputStream extends OutputStream{
 
   private OutputStream out;
-  private IntegerCoder coder;
+  private StringCoder coder;
   public CodecOutputStream(OutputStream out,String code) {
-    this.coder=new IntegerCoder(code);
+    this.coder=new StringCoder(code);
     this.out=out;
   }
   public void write(int c) throws IOException {
-    String s=coder.encode(c);
+  	StringBuilder t=new StringBuilder();
+    String s=coder.encode(t.append((char)c).toString());
     for(Byte b:s.getBytes())
       out.write(b);
   }
